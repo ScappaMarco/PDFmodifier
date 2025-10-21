d857f3f1155c0533bd54e10b175390b16c381582
ScappaMarco <mscappa0@gmail.com>
2025-10-21 18:04:22 +0200

    .

---
+++ PDFmodifier/src/main/java/com/PDFmodifier/PDFmodifier/PdFmodifierApplication.java
@@ -0,0 +1,12 @@
+package com.PDFmodifier.PDFmodifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdFmodifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdFmodifierApplication.class, args);
+	}
+}
---
+++ PDFmodifier/src/test/java/com/PDFmodifier/PDFmodifier/PdFmodifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PDFmodifier.PDFmodifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdFmodifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
